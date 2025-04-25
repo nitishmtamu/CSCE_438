6d38dd0cd7071009ae3b1dd6ea086a669919b783
Nitish Malluru <nitishmalluru@tamu.edu>
2025-04-25 11:14:23 -0500

    fixed

--- mp3/SleepTime.java
+++ mp3/SleepTime.java
@@ -64,15 +64,14 @@ public class SleepTime {
 
         private IntWritable result = new IntWritable();
 
-        public void reduce(Text key, Iterable<IntWritable> values,
-                        Context context
-                        ) throws IOException, InterruptedException {
-        int sum = 0;
-        for (IntWritable val : values) {
-            sum += val.get();
-        }
-        result.set(sum);
-        context.write(key, result);
+        public void reduce(IntWritable key, Iterable<IntWritable> values, Context context)
+                throws IOException, InterruptedException {
+            int sum = 0;
+            for (IntWritable val : values) {
+                sum += val.get();
+            }
+            result.set(sum);
+            context.write(key, result);
         }
     }
 
